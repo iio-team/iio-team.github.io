5ba41d8ee0593898921b75b33027b620741e76f3
Takacs Hunor <tkcshnr@gmail.com>
2023-06-19 22:51:34 +0200

    random image id fix

--- src/routes/gallery/[slug]/+page.server.js
+++ src/routes/gallery/[slug]/+page.server.js
@@ -2,7 +2,7 @@
 export async function load() {
     let images = [];
 	for (let i = 1; i <= 12; i++) {
-		images.push({id: i, name: "Image_".concat(i.toString())});
+		images.push({id: i*12, name: "Image_".concat(i.toString())});
 	}
     return {images};
 };
