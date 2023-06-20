7cfe62718229f216411f01758073aaaed2cd0d32
Takacs Hunor <tkcshnr@gmail.com>
2023-06-20 11:12:02 +0200

    gallery modal adjustments

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -8,7 +8,7 @@
 	<title>Gallery</title>
 </s-head>
 
-<div class="flex flex-col justify-start items-center">
+<div class="flex flex-col justify-start items-center bg-base-100">
 	<div class="hero h-80" style="background-image: url('https://picsum.photos/900/300')">
 		<div class="hero-overlay bg-opacity-60" />
 		<div class="hero-content text-center text-neutral-content">
@@ -21,10 +21,10 @@
 	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-8 py-12">
 		<!-- TODO: create paths with slugs for all the sub-galleries, like: /gallery/eventID -->
 		{#each data.galleries as gallery}
-			<a href="/gallery/{gallery.id}" class="card card-compact w-full lg:w-72 xl:w-96 bg-neutral shadow-xl overflow-hidden">
+			<a href="/gallery/{gallery.id}" class="card card-compact w-full lg:w-72 xl:w-96 bg-base-300 shadow-xl overflow-hidden">
 				<figure><img src="https://picsum.photos/1366/768?random={gallery.id}" class="w-full object-cover" alt="" /></figure>
-				<div class="card-body bg-neutral w-full h-14 transition-all">
-					<h2 class="card-title text-neutral-content">{gallery.name}</h2>
+				<div class="card-body bg-base-300 w-full h-14 transition-all">
+					<h2 class="card-title text-base-content">{gallery.name}</h2>
 				</div>
 			</a>
 		{/each}
--- src/routes/gallery/[slug]/+page.svelte
+++ src/routes/gallery/[slug]/+page.svelte
@@ -4,7 +4,7 @@
 	export let data;
 </script>
 
-<div class="flex flex-col justify-start items-center">
+<div class="flex flex-col justify-start items-center bg-base-100">
 	<div class="hero h-80 bg-accent" style="background-image: url('https://picsum.photos/900/300')">
 		<div class="hero-overlay bg-opacity-60" />
 		<div class="hero-content text-center text-neutral-content">
@@ -18,26 +18,20 @@
 		{#each data.images as image}
 			<button
 				onclick="{image.name}.showModal()"
-				class="card card-compact w-full lg:w-72 xl:w-96 bg-neutral shadow-xl overflow-hidden cursor-pointer"
+				class="card card-compact w-full lg:w-72 xl:w-96 bg-base-300 shadow-2xl overflow-hidden cursor-pointer"
 			>
 				<figure>
 					<img src="https://picsum.photos/1366/768?random={image.id}" alt={image.name} class="w-full object-cover transition-transform"/>
 				</figure>
-				<div class="card-body bg-neutral w-full h-14 p-0 justify-center items-center transition-all">
-					<h2 class="justify-center card-title text-neutral-content">{image.name}</h2>
-				</div>
 			</button>
 
 			<dialog id={image.name} class="modal bg-black bg-opacity-60 backdrop-blur-sm">
-				<form method="dialog" class="modal-box p-0 w-11/12 sm:max-w-xl md:max-w-3xl lg:max-w-5xl xl:max-w-6xl">
+				<form method="dialog" class="modal-box p-0 w-11/12 sm:max-w-xl md:max-w-3xl lg:max-w-5xl xl:max-w-6xl bg-base-300">
 					<img
 						class="w-full objcet-cover"
 						src="https://picsum.photos/1366/768?random={image.id}"
 						alt={image.name}
 					/>
-					<div class="hidden lg:flex modal-action m-4 justify-end">
-						<button class="btn">Close</button>
-					</div> 
 				</form>
 				<form method="dialog" class="modal-backdrop">
 					<button />
@@ -46,9 +40,3 @@
 		{/each}
 	</div>
 </div>
-
-<style>
-	.card:hover figure img {
-		transform: scale(1.1);
-	}
-</style>
