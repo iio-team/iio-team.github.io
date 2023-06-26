cf19b0fbb73e95600c43026e4a2eec41f4f9e66e
Takacs Hunor <tkcshnr@gmail.com>
2023-06-26 13:30:19 +0200

    friend pages description

--- src/routes/friend-pages/+page.svelte
+++ src/routes/friend-pages/+page.svelte
@@ -17,6 +17,19 @@
 	</div>
 </div>
 
+<div class="hero p-8 pb-0">
+	<div class="hero-content text-left">
+		<div class="flex flex-col gap-2 max-w-xl xl:max-w-5xl">
+			<p class="text-lg xl:text-xl">
+				Here is the list of the participating countries and their contact details. 
+			</p>
+			<p class="text-lg xl:text-xl">
+				The contact options are: Leader School website, qualifying competition website, email address.
+			</p>
+		</div>
+	</div>
+</div>
+
 <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 w-full sm:w-fit py-12 px-8 gap-8">
 	{#each data.countries as country}
 		<div class="card items-center w-full sm:w-80 bg-base-300 shadow-xl">
@@ -33,13 +46,25 @@
 				<h2 class="card-title">{country.name}</h2>
 				<div class="card-actions justify-center gap-4">
 					{#if country.school}
-						<a href={country.school} target="_blank" class="btn btn-outline border-2 btn-accent font-bold">School</a>
+						<a
+							href={country.school}
+							target="_blank"
+							class="btn btn-outline border-2 btn-accent font-bold">School</a
+						>
 					{/if}
 					{#if country.iiot}
-						<a href={country.iiot} target="_blank" class="btn btn-outline border-2 btn-primary font-bold">IIOT page</a>
+						<a
+							href={country.iiot}
+							target="_blank"
+							class="btn btn-outline border-2 btn-primary font-bold">IIOT page</a
+						>
 					{/if}
 					{#if country.email}
-						<a href="mailto:{country.email}" target="_blank" class="btn btn-outline border-2 font-bold">Email</a>
+						<a
+							href="mailto:{country.email}"
+							target="_blank"
+							class="btn btn-outline border-2 font-bold">Email</a
+						>
 					{/if}
 				</div>
 			</div>
