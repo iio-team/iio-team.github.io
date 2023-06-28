7e27bc2569c0bcfe5cf3f0540a17907294931612
Takacs Hunor <tkcshnr@gmail.com>
2023-06-28 18:07:12 +0200

    layout min-h-screen for empty pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,13 +6,15 @@
 	let year = new Date().getFullYear();
 </script>
 
-<Navigation></Navigation>
+<div class="flex min-h-screen flex-col">
+	<Navigation />
 
-<main class="w-full flex flex-col items-center bg-neutral">
-	<slot />
-</main>
+	<main class="w-full grow flex flex-col items-center bg-neutral">
+		<slot />
+	</main>
 
-<footer class="footer footer-center h-fit p-8 gap-6 bg-base-300">
-	<img class="max-w-xs opacity-60" src="/images/IIOT.png" alt="IIOT logo" />
-	<p class="text-base-content">Copyright © 2023 - All right reserved</p>
-</footer>
+	<footer class="footer footer-center h-fit p-8 gap-6 bg-base-300">
+		<img class="max-w-xs opacity-60" src="/images/IIOT.png" alt="IIOT logo" />
+		<p class="text-base-content">Copyright © 2023 - All right reserved</p>
+	</footer>
+</div>
