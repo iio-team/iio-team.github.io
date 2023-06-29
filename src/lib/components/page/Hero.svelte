0169d68273840bdfbd13d18dde554803a373264b
Takacs Hunor <tkcshnr@gmail.com>
2023-06-29 12:37:46 +0200

    removed max-w

--- src/lib/components/page/Hero.svelte
+++ src/lib/components/page/Hero.svelte
@@ -1,9 +1,7 @@
 <div class="hero h-52 md:h-64 lg:h-72"> 
 	<div class="hero-content text-center">
-		<div class="max-w-md">
-			<h1 class="text-5xl text-neutral-content font-bold">
-				<slot />
-			</h1>
-		</div>
+		<h1 class="text-5xl text-neutral-content font-bold">
+			<slot />
+		</h1>
 	</div>
 </div>
