f8ac0b85690ad58dde6b674f91bd855d73fdbd5e
Takacs Hunor <tkcshnr@gmail.com>
2023-06-29 09:58:28 +0200

    footer smaller on mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 		<slot />
 	</main>
 
-	<footer class="footer footer-center h-fit py-16 bg-base-300">
+	<footer class="footer footer-center h-16 md:h-24 lg:h-32 bg-base-300">
 		<p class="text-base-content">Copyright © 2023 - All right reserved</p>
 	</footer>
 </div>
