6f95a3b529a630d6033ee47db91e00280e8af71a
Takacs Hunor <tkcshnr@gmail.com>
2023-07-03 16:59:25 +0200

    hero font size fix

--- src/lib/components/page/Hero.svelte
+++ src/lib/components/page/Hero.svelte
@@ -1,6 +1,6 @@
 <div class="hero h-52 md:h-64 lg:h-72"> 
 	<div class="hero-content text-center">
-		<h1 class="text-5xl text-neutral-content font-bold">
+		<h1 class="text-4xl lg:text-5xl text-neutral-content font-bold">
 			<slot />
 		</h1>
 	</div>
