ab109a4fd462aee03bf48b1a83bb0b08545eae12
Takacs Hunor <tkcshnr@gmail.com>
2023-07-05 19:33:06 +0200

    archive page fix

--- src/routes/archive/+page.server.js
+++ src/routes/archive/+page.server.js
@@ -4,10 +4,5 @@ export async function load() {
     const years = Object.keys(competitions);
     years.sort().reverse();
 
-    years.forEach(year => {
-        if(competitions.year.tasks) competitions.year.tasks = {};
-        if(competitions.year.results) competitions.year.results = {};
-    });
-
     return { years, competitions };
 };
