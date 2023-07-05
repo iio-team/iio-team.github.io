1bff2f46703ce67aa79625844c706ecc3526f619
Takacs Hunor <tkcshnr@gmail.com>
2023-07-05 15:38:14 +0200

    empty page placeholder logos

--- src/routes/participate/+page.svelte
+++ src/routes/participate/+page.svelte
@@ -1,3 +1,13 @@
+<script>
+	import Logo from "$lib/components/Logo.svelte";
+</script>
+
 <svelte:head>
 	<title>Participate</title>
 </svelte:head>
+
+<div class="w-full grow flex justify-center items-center bg-base-100">
+    <div class="w-full max-w-3xl p-4">
+		<Logo />
+	</div>
+</div>
