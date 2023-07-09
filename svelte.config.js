39d636b80372ffac70d765068a90ffdf8446422f
Takacs Hunor <tkcshnr@gmail.com>
2023-07-09 17:08:07 +0200

    svelte config fix

--- svelte.config.js
+++ svelte.config.js
@@ -5,15 +5,13 @@ const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+	// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+	// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		kit: {
-			adapter: adapter(),
-			paths: {
-				base: dev ? '' : process.env.BASE_PATH,
-			}
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH,
 		}
 	},
 	preprocess: vitePreprocess()
