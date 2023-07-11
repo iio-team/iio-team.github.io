4147ea60da249de239a83735a40d356e3cc12bc4
Takacs Hunor <tkcshnr@gmail.com>
2023-07-12 01:35:17 +0200

    hero height fix

--- src/lib/components/page/Hero.svelte
+++ src/lib/components/page/Hero.svelte
@@ -1,4 +1,4 @@
-<div class="hero h-40 md:h-52 lg:h-64 py-8 bg-neutral"> 
+<div class="hero h-fit min-h-[10rem] md:min-h-[13rem] lg:min-h-[16rem] py-8 bg-neutral"> 
 	<div class="hero-content text-center">
 		<h1 class="text-4xl lg:text-5xl text-neutral-content font-bold">
 			<slot />
