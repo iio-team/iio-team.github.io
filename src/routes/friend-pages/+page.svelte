c178ce632509526561d1a7d2832f3c1fc52e9910
Takacs Hunor <tkcshnr@gmail.com>
2023-07-11 21:47:16 +0200

    friend pages badge fix

--- src/routes/friend-pages/+page.svelte
+++ src/routes/friend-pages/+page.svelte
@@ -20,7 +20,7 @@
 	<div class="flex flex-col items-center w-full max-w-2xl gap-8">
 		{#each data.countries as country}
 			<div
-				class="card md:card-side min-w-fit w-full justify-center gap-4 md:gap-8 bg-base-200 shadow-lg border border-base-300"
+				class="card md:card-side min-w-fit w-full justify-center gap-8 bg-base-200 shadow-lg border border-base-300"
 			>
 				<figure
 					class="flex-col min-w-fit justify-center items-center gap-4 p-8 pb-0 md:pb-8 md:pr-0"
@@ -32,13 +32,13 @@
 					/>
 					<h1 class="card-title h-fit justify-center text-2xl">{country.name}</h1>
 				</figure>
-				<div class="card-body w-full items-center md:items-start gap-4 p-8 pt-0 md:pt-8 md:pl-0">
+				<div class="card-body w-full md:items-start gap-4 p-8 pt-0 md:pt-8 md:pl-0">
 					{#if country.school}
 						<a
-							class="h-auto pb-2 my-0 box-border btn btn-outline text-lg w-full normal-case flex-col"
+							class="relative h-auto py-2 box-border btn btn-outline text-lg w-full normal-case flex-col"
 							href={country.school.url}
 							>
-							<span id="{country.name}" class="badge -mt-px rounded-t-none rounded-b-lg max-w-fit badge-info transition-none h-auto">
+							<span id="{country.name}" class="badge badge-sm xs:badge-md absolute top-0 -translate-y-full rounded-b-none rounded-t-lg badge-info transition-none h-auto">
 								Leader School / Organization
 							</span>
 							{country.school.name}
