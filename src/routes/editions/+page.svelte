cf3ea758b1f88b97102802ad4cb3c550da6832bd
Takacs Hunor <tkcshnr@gmail.com>
2023-08-07 14:51:51 +0200

    editions page links

--- src/routes/editions/+page.svelte
+++ src/routes/editions/+page.svelte
@@ -14,36 +14,36 @@
 
 <Content>
 	<div class="join join-vertical w-full max-w-2xl shadow-lg rounded-2xl">
-		<AccordionItem year="2023" country="Egypt" tasks results>
-			<p slot="description">
-				The International Final Competition 2023 was held in Port Said (Egypt), on 26-29 of May.
+		<!-- Use the `year` property -->
+		<!-- To display a flag, use the flag="filename" (files inside images/flags) property -->
+		<AccordionItem year="2023" flag="Egypt">
+			<p slot="location">
+				Port Said, Egypt
 			</p>
 		</AccordionItem>
-		<AccordionItem year="2022" country="Italy">
-			<p slot="description">
-				The International Final Competition 2022 was held in Bologna (Italy), on 18-21 of May.
+		<AccordionItem year="2022" flag="Italy">
+			<p slot="location">
+				Bologna, Italy
 			</p>
 		</AccordionItem>
 		<AccordionItem year="2021">
-			<p slot="description">
-				Because of COVID-19 (Coronavirus) pandemic the 2021 Edition was held online.
+			<p slot="location">
+				Because of the COVID-19 pandemic the 2021 Edition was held online.
 			</p>
 		</AccordionItem>
-		<AccordionItem year="2019" country="Russia">
-			<p slot="description">
-				The International Final Competition 2019 has been held in Moscow (Russia), on 20-24 of May.
+		<AccordionItem year="2019" flag="Russia">
+			<p slot="location">
+				Moscow, Russia
 			</p>
 		</AccordionItem>
-		<AccordionItem year="2018" country="Romania">
-			<p slot="description">
-				The International Final Competition 2018 was held in Piatra Neamt (Romania) on Friday the
-				25th of May.
+		<AccordionItem year="2018" flag="Romania">
+			<p slot="location">
+				Piatra Neamt, Romania
 			</p>
 		</AccordionItem>
-		<AccordionItem year="2017" country="Italy">
-			<p slot="description">
-				The International Final Competition 2017 was held in Bologna (Italy) on Friday the 19th of
-				May.
+		<AccordionItem year="2017" flag="Italy">
+			<p slot="location">
+				Bologna, Italy
 			</p>
 		</AccordionItem>
 	</div>
--- src/routes/editions/AccordionItem.svelte
+++ src/routes/editions/AccordionItem.svelte
@@ -1,22 +1,27 @@
+<script context="module">
+	import tasks_file from '$lib/competition-data/tasks.json';
+	import results_file from '$lib/competition-data/results.json';
+</script>
+
 <script>
 	// @ts-nocheck
-
 	import { base } from '$app/paths';
 
 	export let year;
-	export let country = null;
-	export let tasks = null;
-	export let results = null;
+	export let flag = null;
+	let tasks = tasks_file[`${year}`];
+	let results = results_file[`${year}`];
 </script>
 
 <div class="join join-vertical w-full max-w-2xl shadow-lg rounded-2xl">
-	<div class="collapse collapse-arrow join-item bg-base-200 border border-base-300">
+	<div class:collapse-arrow="{tasks || results}" class="collapse join-item bg-base-200 border border-base-300">
 		<input type="radio" name="my-accordion-4" />
 		<div class="collapse-title text-xl font-medium flex gap-6 items-center h-fit">
 			{year}
-			{#if country}
-				<img src="{base}/images/flags/{country}.svg" alt="" class="h-6 rounded-sm" />
+			{#if flag}
+				<img src="{base}/images/flags/{flag}.svg" alt="" class="h-6 rounded-sm" />
 			{/if}
+			<slot name="location"></slot>
 		</div>
 		<div class="collapse-content flex flex-col gap-4">
 			<div class="contents text-lg">
