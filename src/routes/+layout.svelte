905bfe659021e6558ff993f8bcb92a2997b569fb
Takacs Hunor <tkcshnr@gmail.com>
2023-08-07 17:33:12 +0200

    footer + logo pack

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 
 	import Navigation from '$lib/components/Navigation.svelte';
+	import ThemeSwitch from '$lib/components/ThemeSwitch.svelte';
 
 	let year = new Date().getFullYear();
 </script>
@@ -13,7 +14,18 @@
 		<slot />
 	</main>
 
-	<footer class="footer footer-center p-10 bg-base-300 text-base-content gap-4">
-		<p class="text-base-content">Copyright © {year} - All right reserved</p>
+	<footer class="footer gap-6 footer-center p-10 bg-base-300 text-base-content">
+		<div class="flex gap-6">
+			<a href="/images/logo-pack.zip" download class="link link-hover text-base-content">Logo pack</a>
+			
+			<a class="link link-hover" href="mailto:iio.team@iio.team">iio.team@iio.team</a>
+			
+			<a href="https://www.facebook.com/iio.team/" class="text-base-content">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-[#3B5998]"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg>
+			</a>
+		</div>
+		<div>
+			<p class="opacity-80">Copyright © {year} - IIOT</p>
+		</div>
 	</footer>
 </div>
