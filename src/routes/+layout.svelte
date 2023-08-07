60f46db58f736330fbd9618a6d1a05a80ca08000
Takacs Hunor <tkcshnr@gmail.com>
2023-08-07 20:04:56 +0200

    footer link join

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,17 +16,17 @@
 	</main>
 
 	<footer class="footer gap-6 footer-center p-10 bg-base-300 text-base-content">
-		<div class="flex gap-3 flex-wrap justify-center">
-			<a href="{base}/images/logo-pack.zip" download class="link link-hover text-base-content">Logo pack</a>
-			•
-			<a class="link link-hover" href="mailto:iio.team@iio.team">iio.team@iio.team</a>
-			•
-			<a href="https://www.facebook.com/iio.team/" class="text-base-content">
-				<img src="{base}/images/facebook.png" alt="facebook" class="w-6">
+		<div class="join gap-0">
+			<a href="{base}/images/logo-pack.zip" download class="join-item btn normal-case btn-outline border-neutral">Logo pack</a>
+			
+			<a class="join-item btn btn-outline border-neutral normal-case" href="mailto:iio.team@iio.team">iio.team@iio.team</a>
+			
+			<a href="https://www.facebook.com/iio.team/" class="join-item btn btn-outline border-[#3b5998] bg-[#3b5998] hover:bg-[#273b65] hover:border-[#273b65]">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-white"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg>
 			</a>
 		</div>
 		<div>
-			<p class="opacity-80">Copyright © {year} - IIOT</p>
+			<p class="opacity-90">Copyright © {year} - IIOT</p>
 		</div>
 	</footer>
 </div>
