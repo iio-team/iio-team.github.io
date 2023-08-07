3892aa70b405d4b13a70aa9c760f71385c38d724
Takacs Hunor <tkcshnr@gmail.com>
2023-08-07 19:42:32 +0200

    footer facebook logo + bullets

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,13 +16,13 @@
 	</main>
 
 	<footer class="footer gap-6 footer-center p-10 bg-base-300 text-base-content">
-		<div class="flex gap-6">
+		<div class="flex gap-3 flex-wrap justify-center">
 			<a href="{base}/images/logo-pack.zip" download class="link link-hover text-base-content">Logo pack</a>
-			
+			•
 			<a class="link link-hover" href="mailto:iio.team@iio.team">iio.team@iio.team</a>
-			
+			•
 			<a href="https://www.facebook.com/iio.team/" class="text-base-content">
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-[#3B5998]"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg>
+				<img src="{base}/images/facebook.png" alt="facebook" class="w-6">
 			</a>
 		</div>
 		<div>
