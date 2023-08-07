70c4c79f8ef5caeefdb4bcd2219a479c4a0ba063
Takacs Hunor <tkcshnr@gmail.com>
2023-08-07 18:59:37 +0200

    navbar active link

--- src/lib/components/NavList.svelte
+++ src/lib/components/NavList.svelte
@@ -3,6 +3,7 @@
 	import { createEventDispatcher } from 'svelte';
 
 	import { base } from '$app/paths';
+	import { page } from '$app/stores';
 
 	const dispatch = createEventDispatcher();
 
@@ -17,24 +18,24 @@
 	}
 </script>
 
-<li><a href="{base}/" on:click={closeDrawer}>Home</a></li>
+<li><a href="{base}/" class:focus={$page.route.id === `${base}/`} on:click={closeDrawer}>Home</a></li>
 <li>
 	<details bind:open={dropdownOpened} use:clickOutside on:outsideclick={closeDropdown}>
-		<summary>Competition</summary>
+		<summary class:focus={$page.route.id?.includes(`${base}/competition`)}>Competition</summary>
 		<ul class="p-2">
-			<li><a href="{base}/competition/tasks/2023" on:click={closeDrawer}>Tasks</a></li>
-			<li><a href="{base}/competition/results/2023" on:click={closeDrawer}>Results</a></li>
+			<li><a href="{base}/competition/tasks/2023" class:focus={$page.route.id === `${base}/competition/tasks/[year]`} on:click={closeDrawer}>Tasks</a></li>
+			<li><a href="{base}/competition/results/2023" class:focus={$page.route.id === `${base}/competition/results/[year]`} on:click={closeDrawer}>Results</a></li>
 			<li></li>
 			<li><a href="{base}/pdf/Regulations.pdf" target="_blank" on:click={closeDrawer} class="gap-8 min-w-max">Rules <img class="w-6 ml-auto" src="{base}/images/pdf.png" alt="pdf"/></a></li>
 			<li><a href="{base}/pdf/Syllabus.pdf" target="_blank" on:click={closeDrawer} class="gap-8 min-w-max">Syllabus <img class=" w-6 ml-auto" src="{base}/images/pdf.png" alt="pdf"/></a></li>
 		</ul>
 	</details>
 </li>
-<li><a href="{base}/schedule" on:click={closeDrawer}>Schedule</a></li>
-<li><a href="{base}/countries" on:click={closeDrawer}>Countries</a></li>
-<li><a href="{base}/editions" on:click={closeDrawer}>Editions</a></li>
-<li><a href="{base}/gallery" on:click={closeDrawer}>Gallery</a></li>
-<li><a class="lg:border lg:border-base-content" href="{base}/participate" on:click={closeDrawer}>Participate</a></li>
+<li><a href="{base}/schedule" class:focus={$page.route.id === `${base}/schedule`} on:click={closeDrawer}>Schedule</a></li>
+<li><a href="{base}/countries" class:focus={$page.route.id === `${base}/countries`} on:click={closeDrawer}>Countries</a></li>
+<li><a href="{base}/editions" class:focus={$page.route.id === `${base}/editions`} on:click={closeDrawer}>Editions</a></li>
+<li><a href="{base}/gallery" class:focus={$page.route.id?.includes(`${base}/gallery`)} on:click={closeDrawer}>Gallery</a></li>
+<li><a class="lg:border lg:border-base-content"  class:focus={$page.route.id === `${base}/participate`} href="{base}/participate" on:click={closeDrawer}>Participate</a></li>
 
 <style>
 	summary::after {
