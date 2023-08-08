996e4a9140cf0352efbcb1cb963956f5c94b80fb
Takacs Hunor <tkcshnr@gmail.com>
2023-08-08 20:32:43 +0200

    footer facebook logo size

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,7 +22,7 @@
 			<a class="join-item btn btn-outline border-neutral normal-case" href="mailto:iio.team@iio.team">iio.team@iio.team</a>
 			
 			<a href="https://www.facebook.com/iio.team/" class="join-item btn btn-outline border-[#3b5998] bg-[#3b5998] hover:bg-[#273b65] hover:border-[#273b65]">
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-white"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg>
+				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" class="fill-white"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg>
 			</a>
 		</div>
 		<div>
