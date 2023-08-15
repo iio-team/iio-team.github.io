8d3a6d7f08e9eab5bd022df080d02c0ac50699b0
Takacs Hunor <tkcshnr@gmail.com>
2023-08-15 16:03:41 +0200

    footer join button origins

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,11 +17,11 @@
 
 	<footer class="footer gap-6 footer-center p-10 bg-base-300 text-base-content">
 		<div class="join gap-0">
-			<a href="{base}/images/logo-pack.zip" download class="join-item btn normal-case btn-outline border-neutral">Logo pack</a>
+			<a href="{base}/images/logo-pack.zip" download class="join-item origin-right btn normal-case btn-outline border-neutral">Logo pack</a>
 			
 			<a class="join-item btn btn-outline border-neutral normal-case" href="mailto:iio.team@iio.team">iio.team@iio.team</a>
 			
-			<a href="https://www.facebook.com/iio.team/" class="join-item btn btn-outline border-[#3b5998] bg-[#3b5998] hover:bg-[#273b65] hover:border-[#273b65]">
+			<a href="https://www.facebook.com/iio.team/" class="join-item origin-left btn btn-outline border-[#3b5998] bg-[#3b5998] hover:bg-[#273b65] hover:border-[#273b65]">
 				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" class="fill-white"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg>
 			</a>
 		</div>
