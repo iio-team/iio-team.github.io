54b2fd1807d7471ce098e80ebfc797cd65984978
Takacs Hunor <tkcshnr@gmail.com>
2023-08-22 00:35:32 +0200

    fontawesome credits

--- src/routes/gallery/[year]/+page.svelte
+++ src/routes/gallery/[year]/+page.svelte
@@ -56,8 +56,15 @@
 				method="dialog"
 				class="modal-box p-0 w-full h-fit md:w-fit md:h-full max-w-full max-h-[90%] bg-base-300 relative"
 			>
-				<button class="btn border-none opacity-60 hover:opacity-100 transition-opacity w-9 h-9 min-h-0 btn-square rounded-2xl rounded-tl-none rounded-br-none absolute right-0 top-0 origin-top-right">
-					<svg xmlns="http://www.w3.org/2000/svg" class="h-6 fill-base-content" viewBox="0 0 384 512">
+				<button
+					class="btn border-none opacity-60 hover:opacity-100 transition-opacity w-9 h-9 min-h-0 btn-square rounded-2xl rounded-tl-none rounded-br-none absolute right-0 top-0 origin-top-right"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-6 fill-base-content"
+						viewBox="0 0 384 512"
+					>
+						<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
 						<path
 							d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
 						/>
@@ -81,7 +88,12 @@
 						on:click={previous}
 						class="btn border-none opacity-60 hover:opacity-100 transition-opacity btn-square w-9 h-9 min-h-0 rounded-tl-none rounded-bl-none origin-left text-lg"
 					>
-						<svg xmlns="http://www.w3.org/2000/svg" class="h-7 fill-base-content" viewBox="0 0 256 512">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-7 fill-base-content"
+							viewBox="0 0 256 512"
+						>
+							<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
 							<path
 								d="M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 256c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-128-128z"
 							/>
@@ -94,7 +106,12 @@
 						on:click={next}
 						class="btn border-none opacity-60 hover:opacity-100 transition-opacity btn-square w-9 h-9 min-h-0 rounded-tr-none rounded-br-none origin-right text-lg"
 					>
-						<svg xmlns="http://www.w3.org/2000/svg" class="h-7 fill-base-content" viewBox="0 0 256 512">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-7 fill-base-content"
+							viewBox="0 0 256 512"
+						>
+							<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
 							<path
 								d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z"
 							/>
