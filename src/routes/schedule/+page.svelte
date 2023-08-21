831114d93d07c7db1e907f1a325e0e74fdf712e7
Takacs Hunor <tkcshnr@gmail.com>
2023-08-22 00:35:24 +0200

    schedule

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,13 +1,54 @@
 <script>
-	import Logo from "$lib/components/Logo.svelte";
+	import Hero from '$lib/components/page/Hero.svelte';
+	import Content from '$lib/components/page/Content.svelte';
+	import Calendar from './Calendar.svelte';
 </script>
 
 <svelte:head>
 	<title>Schedule</title>
 </svelte:head>
 
-<div class="w-full grow flex justify-center items-center bg-base-100">
-    <div class="w-full max-w-3xl p-4">
-		<Logo />
+<Hero>Schedule</Hero>
+
+<Content>
+	<div class="card max-w-xl w-full bg-base-200 shadow-xl">
+		<div class="card-body text-center">
+			<h2 class="text-xl divider">1st qualifying round</h2>
+			<div class="flex justify-center gap-4">
+				<Calendar />
+				<p class="text-lg grow-0">13 November 2023</p>
+			</div>
+			<h2 class="text-xl divider">2nd qualifying round</h2>
+			<div class="flex justify-center gap-4">
+				<Calendar />
+				<p class="text-lg grow-0">12 December 2023</p>
+			</div>
+			<h2 class="text-xl divider">3rd qualifying round</h2>
+			<div class="flex justify-center gap-4">
+				<Calendar />
+				<p class="text-lg grow-0">17 January 2024</p>
+			</div>
+			<h2 class="text-xl divider">4th qualifying round</h2>
+			<div class="flex justify-center gap-4">
+				<Calendar />
+				<p class="text-lg grow-0">8 February 2024</p>
+			</div>
+			<h2 class="text-xl divider">National finals</h2>
+			<div class="flex justify-center gap-4">
+				<Calendar />
+				<p class="text-lg grow-0">March, 2024</p>
+			</div>
+			<h2 class="text-xl divider">International finals</h2>
+			<div class="flex justify-center gap-4">
+				<Calendar />
+				<p class="text-lg grow-0">May, 2024</p>
+			</div>
+		</div>
 	</div>
-</div>
+</Content>
+
+<style>
+	h2 {
+		font-weight: bold;
+	}
+</style>
