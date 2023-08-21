8ba8fb03ffce057426a2fe348b2ccbc0601cdcb5
Takacs Hunor <tkcshnr@gmail.com>
2023-08-22 00:43:32 +0200

    tasks button secondary

--- src/routes/competition/tasks/[year]/+page.svelte
+++ src/routes/competition/tasks/[year]/+page.svelte
@@ -23,7 +23,7 @@
 					<h2 class="card-title text-xl">{task.name} ({task.id})</h2>
 					<div class="card-actions items-stretch gap-4">
 						<a
-							class="text-lg normal-case h-auto px-4 py-2 btn btn-primary grow w-fit"
+							class="text-lg normal-case h-auto px-4 py-2 btn btn-secondary grow w-fit"
 							href="{base}/tasks/2023/{task.id}/english.pdf"
 							target="_blank"
 						>
