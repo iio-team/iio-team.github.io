70b776be105f7bc1d8fd9cc4527b1ad432a9c368
Takacs Hunor <tkcshnr@gmail.com>
2023-08-22 13:28:24 +0200

    hero small shadow

--- src/lib/components/page/Hero.svelte
+++ src/lib/components/page/Hero.svelte
@@ -1,4 +1,4 @@
-<div class="hero h-fit min-h-[10rem] md:min-h-[13rem] lg:min-h-[16rem] py-8 bg-base-200 border-b border-base-300"> 
+<div class="hero h-fit min-h-[10rem] md:min-h-[13rem] lg:min-h-[16rem] py-8 bg-base-200 border-b border-base-300 shadow-sm z-10"> 
 	<div class="hero-content text-center">
 		<h1 class="text-4xl lg:text-5xl text-base-content font-bold">
 			<slot />
