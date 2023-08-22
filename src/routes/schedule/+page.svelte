ef2c8e18dc549b44c4e3941bc72c3547c093a92a
Takacs Hunor <tkcshnr@gmail.com>
2023-08-22 13:28:46 +0200

    schedule with date component

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -2,6 +2,7 @@
 	import Hero from '$lib/components/page/Hero.svelte';
 	import Content from '$lib/components/page/Content.svelte';
 	import Calendar from './Calendar.svelte';
+	import Date from './Date.svelte';
 </script>
 
 <svelte:head>
@@ -12,37 +13,31 @@
 
 <Content>
 	<div class="card max-w-xl w-full bg-base-200 shadow-xl">
-		<div class="card-body text-center">
-			<h2 class="text-xl divider">1st qualifying round</h2>
-			<div class="flex justify-center gap-4">
-				<Calendar />
-				<p class="text-lg grow-0">13 November 2023</p>
-			</div>
-			<h2 class="text-xl divider">2nd qualifying round</h2>
-			<div class="flex justify-center gap-4">
-				<Calendar />
-				<p class="text-lg grow-0">12 December 2023</p>
-			</div>
-			<h2 class="text-xl divider">3rd qualifying round</h2>
-			<div class="flex justify-center gap-4">
-				<Calendar />
-				<p class="text-lg grow-0">17 January 2024</p>
-			</div>
-			<h2 class="text-xl divider">4th qualifying round</h2>
-			<div class="flex justify-center gap-4">
-				<Calendar />
-				<p class="text-lg grow-0">8 February 2024</p>
-			</div>
-			<h2 class="text-xl divider">National finals</h2>
-			<div class="flex justify-center gap-4">
-				<Calendar />
-				<p class="text-lg grow-0">March, 2024</p>
-			</div>
-			<h2 class="text-xl divider">International finals</h2>
-			<div class="flex justify-center gap-4">
-				<Calendar />
-				<p class="text-lg grow-0">May, 2024</p>
-			</div>
+		<div class="card-body text-center gap-6">
+			<Date>
+				<h2 slot="title">1st qualifying round</h2>
+				<p slot="date">13 November 2023</p>
+			</Date>
+			<Date>
+				<h2 slot="title">2nd qualifying round</h2>
+				<p slot="date">12 December 2023</p>
+			</Date>
+			<Date>
+				<h2 slot="title">3rd qualifying round</h2>
+				<p slot="date">17 January 2024</p>
+			</Date>
+			<Date>
+				<h2 slot="title">4th qualifying round</h2>
+				<p slot="date">8 February 2024</p>
+			</Date>
+			<Date>
+				<h2 slot="title">National finals</h2>
+				<p slot="date">March, 2024</p>
+			</Date>
+			<Date>
+				<h2 slot="title">International finals</h2>
+				<p slot="date">May, 2024</p>
+			</Date>
 		</div>
 	</div>
 </Content>
---
+++ src/routes/schedule/Date.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Calendar from "./Calendar.svelte";
+</script>
+
+<div class="flex flex-col gap-2">
+    <div class="text-xl divider">
+        <slot name="title"></slot>
+    </div>
+    <div class="flex justify-center gap-4">
+        <Calendar />
+        <div class="text-lg grow-0">
+            <slot name="date"></slot>
+        </div>
+    </div>
+</div>
