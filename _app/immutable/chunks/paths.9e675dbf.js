64ac104ac666d1da762e02d9bd04a86a720153a8
Takacs Hunor <tkcshnr@gmail.com>
2023-08-23 16:31:01 +0200

    Updates

---
+++ _app/immutable/chunks/paths.9e675dbf.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1hoeoqa)==null?void 0:s.base)??"/iio-team.github.io";var a;const o=((a=globalThis.__sveltekit_1hoeoqa)==null?void 0:a.assets)??e;export{o as a,e as b};
---
+++ _app/immutable/nodes/11.3f511b6a.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as o}from"../chunks/index.f6098fd6.js";function r(e){throw new Error("505")}class i extends s{constructor(n){super(),t(this,n,r,null,o,{})}}export{i as component};
