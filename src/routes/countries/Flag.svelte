8ee76e510cc7528abf41bc8aeaaf4b5b2df3873a
Takacs Hunor <tkcshnr@gmail.com>
2023-08-26 22:52:01 +0200

    flag placeholder working

--- src/routes/countries/Flag.svelte
+++ src/routes/countries/Flag.svelte
@@ -4,10 +4,17 @@
 	import { base } from '$app/paths';
 
 	export let country;
+
+    let img;
+    function error() {
+        img.src =`${base}/images/flags/Placeholder.svg`;
+    }
 </script>
 
 <img
+    bind:this={img}
+	on:error={error}
 	class="w-20 md:w-32 rounded-md md:rounded-lg"
-	src="{base}/images/flags/{country.name}.svg"
+	src="{base}/images/flags/{country.name}.svg?{Math.random()}"
 	alt="flag"
 />
