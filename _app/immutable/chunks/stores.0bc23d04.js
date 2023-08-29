3db1979b399c1f1f658bc67b01333e3f1d31db12
Takacs Hunor <tkcshnr@gmail.com>
2023-08-29 11:21:59 +0200

    Updates

--- _app/immutable/chunks/stores.0bc23d04.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./singletons.c8e66764.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
