35fbc42f7bd52d5291fea2fbae22eefb908c1ecc
Takacs Hunor <tkcshnr@gmail.com>
2023-08-29 20:40:58 +0200

    contacts

--- src/routes/countries/+page.svelte
+++ src/routes/countries/+page.svelte
@@ -34,7 +34,7 @@
 						<Flag {country} />
 					</div>
 				</figure>
-				<div class="card-body w-full md:items-start gap-4 p-8 pt-0 md:pt-8 md:pl-0">
+				<div class="card-body w-full md:items-start justify-center gap-4 p-8 pt-0 md:pt-8 md:pl-0">
 					{#if country.school}
 						<a
 							class="group relative h-auto py-2 box-border border-none btn btn-neutral text-lg w-full normal-case flex-col"
@@ -55,59 +55,63 @@
 						</a>
 					{/if}
 
-					<div class="divider m-0 mt-2 font-bold">
-						<span class="-mr-2">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="h-5 fill-base-content"
-								viewBox="0 0 512 512"
-							>
-								<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-								<path
-									d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"
-								/>
-							</svg>
-						</span>
-						National referent
-					</div>
-					<div class="w-full flex flex-wrap">
-						<p class="text-lg text-center">
-							{country.national_referent.name || 'Referent Name'},
-							<a
-								href="mailto:{country.national_referent.email || ''}"
-								class="btn-link link-secondary font-semibold"
-							>
-								{country.national_referent.email || 'referent-email@gmail.com'}
-							</a>
-						</p>
-					</div>
+					{#if country.national_referent.email}
+						<div class="divider m-0 mt-2 font-bold">
+							<span class="-mr-2">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									class="h-5 fill-base-content"
+									viewBox="0 0 512 512"
+								>
+									<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+									<path
+										d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"
+									/>
+								</svg>
+							</span>
+							National referent
+						</div>
+						<div class="w-full flex flex-wrap">
+							<p class="text-lg text-center">
+								{country.national_referent.name || ''},
+								<a
+									href="mailto:{country.national_referent.email || ''}"
+									class="btn-link link-secondary font-semibold"
+								>
+									{country.national_referent.email || ''}
+								</a>
+							</p>
+						</div>
+					{/if}
 
-					<div class="divider m-0 font-bold">
-						<span class="-mr-2">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="h-5 fill-base-content"
-								viewBox="0 0 512 512"
-							>
-								<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-								<path
-									d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"
-								/>
-							</svg>
-						</span>
-						Scientific coordinator
-					</div>
-					<div class="w-full flex flex-wrap">
-						<p class="text-lg text-center">
-							{country.scientific_coordinator.name || 'Coordinator Name'},
-							<a
-								href="mailto:{country.scientific_coordinator.email || ''}"
-								class="btn-link link-secondary font-semibold"
-							>
-								{country.scientific_coordinator.email || 'coordinator-email@gmail.com'}
-							</a>
-						</p>
-					</div>
+					{#if country.scientific_coordinator.email}
+						<div class="divider m-0 font-bold">
+							<span class="-mr-2">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									class="h-5 fill-base-content"
+									viewBox="0 0 512 512"
+								>
+									<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+									<path
+										d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"
+									/>
+								</svg>
+							</span>
+							Scientific coordinator
+						</div>
+						<div class="w-full flex flex-wrap">
+							<p class="text-lg text-center">
+								{country.scientific_coordinator.name},
+								<a
+									href="mailto:{country.scientific_coordinator.email}"
+									class="btn-link link-secondary font-semibold"
+								>
+									{country.scientific_coordinator.email}
+								</a>
+							</p>
+						</div>
+					{/if}
 				</div>
 			</div>
 		{/each}
