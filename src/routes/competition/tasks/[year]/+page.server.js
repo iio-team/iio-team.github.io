5470a0c69ada537c6e8ae3895759920785bbe301
Takacs Hunor <tkcshnr@gmail.com>
2023-08-29 18:42:05 +0200

    new task system

--- src/routes/competition/tasks/[year]/+page.server.js
+++ src/routes/competition/tasks/[year]/+page.server.js
@@ -1,27 +1,6 @@
 // @ts-nocheck
 import tasks_json from '$lib/json-data/tasks.json';
-import axios from 'axios';
-import { API_KEY } from '$env/static/private';
 import { error } from '@sveltejs/kit';
-
-// Creating download links for all the zip files in the google drive folder
-// The FOLDER_ID should be specified in the 'tasks.json' file for each year
-async function zipDownloadLinks(FOLDER_ID) {
-    const apiUrl = `https://www.googleapis.com/drive/v3/files?q='${FOLDER_ID}'+in+parents&key=${API_KEY}`;
-    const zipMap = {};
-    try {
-        const response = await axios.get(apiUrl);
-        const files = response.data.files;
-
-        files.forEach(file => {
-            let downloadLink = `https://drive.google.com/uc?id=${file.id}`;
-            zipMap[(file.name.slice(0, file.name.lastIndexOf(".")))] = downloadLink;
-        });
-    } catch (error) {
-        console.error('Error listing files:', error.message);
-    }
-    return zipMap;
-}
 
 export async function load({ params }) {
     const year = params.year;
@@ -30,10 +9,8 @@ export async function load({ params }) {
         throw error(404, "Not Found");
     }
 
-    const tasks = tasks_json[`${year}`].tasks;
-    const FOLDER_ID = tasks_json[`${year}`].FOLDER_ID;
+    let contest_tasks = tasks_json[`${year}`].contest_tasks;
+    let practice_tasks = tasks_json[`${year}`].practice_tasks;
 
-    let zipMap = zipDownloadLinks(FOLDER_ID);
-
-    return { tasks, zipMap, year }
+    return { contest_tasks, practice_tasks, year }
 };
---
+++ src/routes/competition/tasks/[year]/TaskCard.svelte
@@ -0,0 +1,36 @@
+<script>
+	// @ts-nocheck
+	import { base } from '$app/paths';
+
+	export let task;
+</script>
+
+<div class="card card-c bg-base-200 shadow-md border border-base-300">
+	<div class="card-body gap-4 p-8">
+		<h2 class="card-title text-xl">{task.name} ({task.id})</h2>
+		<div class="card-actions items-stretch gap-4 grid grid-cols-1 sm:grid-cols-2">
+			<a
+				class="text-lg normal-case h-auto px-4 py-2 btn btn-neutral col-span-1 sm:col-span-2"
+				href={task.statement}
+				target="_blank"
+			>
+				<img class="w-6" src="{base}/images/pdf.png" alt="pdf" />
+				Statement
+			</a>
+			<a
+				class="text-lg normal-case h-auto px-4 py-2 btn btn-primary col-span-1"
+				href={task.solutions}
+				target="_blank"
+			>
+				Solutions
+			</a>
+			<a
+				class="text-lg normal-case h-auto px-4 py-2 btn btn-secondary col-span-1"
+				href={task.testcases}
+				target="_blank"
+			>
+				Test cases
+			</a>
+		</div>
+	</div>
+</div>
