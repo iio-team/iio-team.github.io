055cd7aff77954a07b37fafc3021cc50493288b6
Takacs Hunor <tkcshnr@gmail.com>
2023-09-10 15:04:34 +0200

    Updates

---
+++ _app/immutable/chunks/paths.a0ee7d59.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_ne8l88)==null?void 0:s.base)??"/iio-team.github.io";var e;const a=((e=globalThis.__sveltekit_ne8l88)==null?void 0:e.assets)??t;export{a,t as b};
