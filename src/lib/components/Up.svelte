240286ea79c8d0a6fd1b7a2fa19c3d0afb79e9bc
Takacs Hunor <tkcshnr@gmail.com>
2023-09-12 09:48:01 +0200

    up button

--- src/lib/components/Up.svelte
+++ src/lib/components/Up.svelte
@@ -22,9 +22,9 @@
 <button
 	on:click={scrollTop}
     class:hidden
-	class="z-50 fixed top-2 h-10 w-10 min-h-0 mx-auto btn btn-circle btn-ghost lg:!hidden"
+	class="z-50 fixed top-4 right-0 ml-auto opacity-100 btn btn-neutral btn-sm btn-square rounded-r-none lg:!hidden"
 >
-	<svg xmlns="http://www.w3.org/2000/svg" class="h-6 fill-base-content" viewBox="0 0 448 512"
+	<svg xmlns="http://www.w3.org/2000/svg" class="h-5 fill-neutral-content" viewBox="0 0 448 512"
 		><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
 			d="M246.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 109.3 361.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160zm160 352l-160-160c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 301.3 361.4 438.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3z"
 		/></svg
