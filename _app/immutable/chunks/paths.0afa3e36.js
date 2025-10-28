e27067bef4178341044d1da62c631c6fd7a2bf43
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2025-10-28 19:10:59 +0100

    Updates

--- _app/immutable/chunks/paths.0afa3e36.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_nmokhh)==null?void 0:s.base)??"/iio-team.github.io";var t;const e=((t=globalThis.__sveltekit_nmokhh)==null?void 0:t.assets)??a;export{e as a,a as b};
