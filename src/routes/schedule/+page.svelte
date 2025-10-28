260b4e92bce21a53541c9dda29411d2c3048bfaa
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2025-10-28 19:09:40 +0100

    update schedule

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -13,35 +13,34 @@
 
 <Content>
 	<Description>
-		<p>The International Final of IIOT 2025 will be held from May 30th to June 3rd in Budapest, Hungary.
-		The website of that event is: <a href="https://iiot2025.hu/" class="btn-link link-secondary font-bold">https://iiot2025.hu/</a></p>
+		<p>The International Final of IIOT 2026 will take place in Piatra Neamț, Romania, in the second half of May 2026.</p>
 		<p>Below is the schedule for the Italian, Romanian and Hungarian qualifiers. Each regular member can
 		have its <span class="font-bold">own schedule</span> for their national competition.</p>
 	</Description>
 	<div class="max-w-xl w-full flex flex-col text-center gap-4">
 		<Date>
 			<h2 slot="title">1st qualifying round</h2>
-			<p slot="date">November 11, 2024</p>
+			<p slot="date">November 10, 2025</p>
 		</Date>
 		<Date>
 			<h2 slot="title">2nd qualifying round</h2>
-			<p slot="date">December 10, 2024</p>
+			<p slot="date">December 9, 2025</p>
 		</Date>
 		<Date>
 			<h2 slot="title">3rd qualifying round</h2>
-			<p slot="date">January 15, 2025</p>
+			<p slot="date">January 14, 2026</p>
 		</Date>
 		<Date>
 			<h2 slot="title">4th qualifying round</h2>
-			<p slot="date">February 6, 2025</p>
+			<p slot="date">February 12, 2026</p>
 		</Date>
 		<Date>
 			<h2 slot="title">National finals</h2>
-			<p slot="date">March, 2025</p>
+			<p slot="date">March, 2026</p>
 		</Date>
 		<Date>
 			<h2 slot="title">International finals</h2>
-			<p slot="date">May 30 - June 3, 2025</p>
+			<p slot="date">May, 2026</p>
 		</Date>
 	</div>
 </Content>
